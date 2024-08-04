3a61a8f382c941b9cbe1824cbdf8544884f9de90
jpang32 <jpangallozzi@gmail.com>
2024-08-03 22:28:56 -0400

    feat: format of output

--- fahrenheit_to_celsius.c
+++ fahrenheit_to_celsius.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main() {
+
+    printf("FARENHEIT | CELSIUS\n");
+    printf("-------------------\n");
     // Loop through i from 0 to 300, incrementing by 20
     for (int i = 0; i < 320; i = i + 20) {
         // Calculate celsius using farenheit
@@ -9,7 +12,7 @@ int main() {
 
         // Print out results in two columns
         // Farenheit left-justified with 3 character column width
-        printf("%-3d %d", i, (int) celsius);
+        printf("%-10d|%8.2f", i, celsius);
         printf("\n");
     }
 }
