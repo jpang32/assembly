c0ddd1ca05edef86a413154704af1797cf19ec01
jpang32 <jpangallozzi@gmail.com>
2024-08-05 21:44:16 -0400

    refactor: use funcs for celsius fahrenheit scripts

--- celsius_to_farenheit.c
+++ celsius_to_farenheit.c
@@ -4,6 +4,8 @@
 #define UPPER 320
 #define STEP 20
 
+double celsius_to_farenheit(double celsius);
+
 int main() {
 
     printf("CELSIUS | FARENHEIT\n");
@@ -13,10 +15,14 @@ int main() {
     float farenheit;
 
     while (celsius <= UPPER) {
-        farenheit = (9.0 / 5.0) * celsius + 32.0;
+        farenheit = celsius_to_farenheit(celsius);
 
         printf("%-8d|%10.2f\n", celsius, farenheit);
 
         celsius += STEP;
     }
+}
+
+double celsius_to_farenheit(double celsius) {
+    return (9.0 / 5.0) * celsius + 32.0;
 }
--- fahrenheit_to_celsius.c
+++ fahrenheit_to_celsius.c
@@ -4,6 +4,8 @@
 #define UPPER 320
 #define STEP 20
 
+double farenheit_to_celsius(double farenheit);
+
 int main() {
 
     printf("FARENHEIT | CELSIUS\n");
@@ -12,11 +14,15 @@ int main() {
     for (int i = LOWER; i < UPPER; i = i + 20) {
         // Calculate celsius using farenheit
         // 5.0 necessary in order to avoid integer arithmetic
-        double celsius = (5.0 / 9) * (i - 32);
+        double celsius = farenheit_to_celsius(i);
 
         // Print out results in two columns
         // Farenheit left-justified with 3 character column width
         printf("%-10d|%8.2f", i, celsius);
         printf("\n");
     }
+}
+
+double farenheit_to_celsius(double farenheit) {
+    return (5.0 / 9) * (farenheit - 32);
 }
