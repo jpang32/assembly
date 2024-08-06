49fd8dabf887a597b9c9a0f29311780ebf97d648
jpang32 <jpangallozzi@gmail.com>
2024-08-05 20:40:19 -0400

    refactor: cleaner blank char reduction program

--- character_io.c
+++ character_io.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
+# define IN 1
+# define OUT 0
+
 int main() {
 
     int c;
-    int is_blank = 0;
+    int state = IN;
 
     while ((c = getchar()) != EOF) {
-        if (c == ' ') {
-            if (!is_blank) {
-                is_blank = 1;
-                printf("%c", c);
-            }
-        } else {
-            is_blank = 0;
+        if (state == IN) {
             printf("%c", c);
+        }
+        if (c == ' ') {
+            state = OUT;
+        } else if (state == OUT) {
+            state = IN;
         }
     }
 
