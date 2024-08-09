6d7124ea4b2fe48cd12918cbc08695f3bfedb3da
jpang32 <jpangallozzi@gmail.com>
2024-08-09 16:20:51 -0400

    WIP: fix print_long_lines to immediately quit when EOF is reached

--- print_long_lines.c
+++ print_long_lines.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-#define THRESHOLD 80
-#define MAX_LEN 200
+#define THRESHOLD 10
+#define MAX_LEN 15
 
 int get_line(char line[], int max_len);
 
@@ -10,26 +10,46 @@ int main() {
     // Read lines from std input
     // If a line is longer than 80 characters, print it
     // If not, ignore and move on
-    int len;
+    int c, i, len;
     char line[MAX_LEN];
 
     // Had issue with old program where it wouldn't quit immediately after 
     // giving EOF signal
     // This is because get_line would return a val > 0, so this while would continue
     // TODO: find a way to fix above
-    while ((len = get_line(line, MAX_LEN)) > 0) {
-        if (len > THRESHOLD) {
+    i = 0;
+    while ((c = getchar()) != EOF) {
+        if (i < MAX_LEN - 1) {
+            line[i] = c;
+            if (c == '\n') {
+                line[++i] = '\0';
+            }
+            
+            if (i > THRESHOLD) {
+                printf("%s", line);
+            }
+            i = -1;
+        } else {
+            line[i] = '\0';
             printf("%s", line);
+            i = -1;
         }
+
+        ++i;
     }
+
+    if (c == EOF) {
+        printf("%s", line);
+    }
+
+
 
     return 0;
 }
 
 int get_line(char line[], int max_len) {
 
-    int c;
-    int i;
+    int c, i;
 
     for (i = 0; i < max_len - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
         line[i] = c;
